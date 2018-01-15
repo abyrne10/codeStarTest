bad4e9fefdf5a161554884b60bcfdc4a682a6088
Alastair Byrne <alastairbyrne@gmail.com>
2018-01-15 11:38:41 +0000

    Added test to test testing

---
+++ tests/test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+describe('test module', () => {
+    it('should pass the test', () => {
+        expect(true).to.equal(true);
+    });
+});

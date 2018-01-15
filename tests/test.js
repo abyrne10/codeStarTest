07882ceec5fb5fa670674fdfc1f9ccc8a620ccaa
Alastair Byrne <alastairbyrne@gmail.com>
2018-01-15 11:55:51 +0000

    Added failing test

--- tests/test.js
+++ tests/test.js
@@ -6,4 +6,7 @@ describe('test module', () => {
     it('should pass the test', () => {
         expect(true).to.equal(true);
     });
+    it('should fail the test', () => {
+        expect(true).to.equal(false);
+    });
 });
